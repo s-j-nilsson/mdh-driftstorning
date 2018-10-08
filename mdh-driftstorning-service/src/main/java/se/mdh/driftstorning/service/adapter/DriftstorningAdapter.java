3409cbd3efb0d442d768cf75399a170da8bdcf2a
Johan Nilsson <s.j.nilsson@gmail.com>
2018-10-08 13:59:24 +0200

    Hanterar fall då datum inte är satt

--- mdh-driftstorning-service/src/main/java/se/mdh/driftstorning/service/adapter/DriftstorningAdapter.java
+++ mdh-driftstorning-service/src/main/java/se/mdh/driftstorning/service/adapter/DriftstorningAdapter.java
@@ -1,5 +1,6 @@
 package se.mdh.driftstorning.service.adapter;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import javax.validation.constraints.NotNull;
 import org.springframework.stereotype.Component;
@@ -14,8 +15,9 @@ public class DriftstorningAdapter {
     driftstorning.setNiva(Driftstorning.Niva.ERROR);
     driftstorning.setOrsakskod(driftstorningPost.getAnledningPost().getNamn());
     driftstorning.setMeddelandeSv(driftstorningPost.getAnledningPost().getMeddelande());
-    driftstorning.setStart(LocalDateTime.of(driftstorningPost.getStartDatum(), driftstorningPost.getStartTid()));
-    driftstorning.setSlut(LocalDateTime.of(driftstorningPost.getSlutDatum(), driftstorningPost.getSlutTid()));
+    driftstorning.setStart(LocalDateTime.of(driftstorningPost.getStartDatum() != null ? driftstorningPost.getStartDatum() : LocalDate.now(),
+                                            driftstorningPost.getStartTid()));
+    driftstorning.setSlut(LocalDateTime.of(driftstorningPost.getSlutDatum() != null ? driftstorningPost.getSlutDatum() : LocalDate.now(), driftstorningPost.getSlutTid()));
     return driftstorning;
   }
 }
