cbb8cd7088776a10d2730093234c45d67f62fe3e
johan Nilsson <s.j.nilsson@gmail.com>
2018-10-08 21:12:23 +0200

    Initiera MongoDb med data

---
+++ mdh-driftstorning-admin/src/main/java/se/mdh/driftstorning/admin/init/MongoDbInitializer.java
@@ -0,0 +1,69 @@
+package se.mdh.driftstorning.admin.init;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import se.mdh.driftstorning.admin.repository.AnledningRepository;
+import se.mdh.driftstorning.admin.repository.KanalRepository;
+import se.mdh.driftstorning.common.model.AnledningPost;
+import se.mdh.driftstorning.common.model.KanalPost;
+
+@Component
+@Profile("init-mongo-data")
+public class MongoDbInitializer implements CommandLineRunner {
+
+  private KanalRepository kanalRepository;
+  private AnledningRepository anledningRepository;
+
+  public MongoDbInitializer(KanalRepository kanalRepository, AnledningRepository anledningRepository) {
+    this.kanalRepository = kanalRepository;
+    this.anledningRepository = anledningRepository;
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    if(kanalRepository.findAll().isEmpty()) {
+      KanalPost kanalPost1 = new KanalPost();
+      kanalPost1.setNamn("ladok");
+      kanalPost1.setText("Ladok");
+      kanalRepository.save(kanalPost1);
+
+      KanalPost kanalPost2 = new KanalPost();
+      kanalPost2.setNamn("mdh");
+      kanalPost2.setText("Mdh");
+      kanalRepository.save(kanalPost2);
+
+      KanalPost kanalPost3 = new KanalPost();
+      kanalPost3.setNamn("selma");
+      kanalPost3.setText("Selma");
+      kanalRepository.save(kanalPost3);
+
+      KanalPost kanalPost4 = new KanalPost();
+      kanalPost4.setNamn("kronox");
+      kanalPost4.setText("Kronox");
+      kanalRepository.save(kanalPost4);
+    }
+
+    if(anledningRepository.findAll().isEmpty()) {
+      AnledningPost anledningPost1 = new AnledningPost();
+      anledningPost1.setNamn("uppgradering");
+      anledningPost1.setText("Uppgradering");
+      anledningRepository.save(anledningPost1);
+
+      AnledningPost anledningPost2 = new AnledningPost();
+      anledningPost2.setNamn("backup");
+      anledningPost2.setText("Backup");
+      anledningRepository.save(anledningPost2);
+
+      AnledningPost anledningPost3 = new AnledningPost();
+      anledningPost3.setNamn("patchning");
+      anledningPost3.setText("Patchning");
+      anledningRepository.save(anledningPost3);
+
+      AnledningPost anledningPost4 = new AnledningPost();
+      anledningPost4.setNamn("prestanda");
+      anledningPost4.setText("Prestandaproblem");
+      anledningRepository.save(anledningPost4);
+    }
+  }
+}
